b706ce44cd31c90317e12c3895101d4c3565e4a7
Josef Kuchař <josef.kuchar267@gmail.com>
2018-12-03 15:28:32 +0100

    Refactor

--- src/day3.ts
+++ src/day3.ts
@@ -14,26 +14,22 @@ class Area {
     }
 }
 
-export function part1(areas: Area[]) {
-    const width = 1000;
-    let fabric = new Array(width * width).fill(0);
+export function solve(input: string) {
+    // Parse input
+    const areas = input
+        .split("\n")
+        .map(x =>
+            x
+                .substring(1)
+                .split(/,|x|: | @/)
+                .map(y => parseInt(y))
+        )
+        .map(x => new Area(x[0], x[1], x[2], x[3], x[4]));
 
-    areas.forEach(area => {
-        for (let x = 0; x < area.w; x++) {
-            for (let y = 0; y < area.h; y++) {
-                fabric[width * (area.x + x) + (area.y + y)]++;
-            }
-        }
-    });
-
-    return fabric.reduce((x, y) => (y > 1 ? x + 1 : x));
-}
-
-export function part2(areas: Area[]) {
     const width = 1000;
     let fabric: number[][] = new Array(width * width);
     let areaIds = new Set();
-    
+
     for (let i = 0; i < fabric.length; i++) {
         fabric[i] = new Array();
     }
@@ -56,23 +52,10 @@ export function part2(areas: Area[]) {
         }
     });
 
-    return areaIds.values().next().value;
-}
+    let collisions = 0;
+    fabric.forEach(piece => {
+        if (piece.length > 1) collisions++;
+    });
 
-export function solve(input: string) {
-    // Parse input
-    const areas = input
-        .split("\n")
-        .map(x =>
-            x
-                .substring(1)
-                .split(/,|x|: | @/)
-                .map(y => parseInt(y))
-        )
-        .map(x => new Area(x[0], x[1], x[2], x[3], x[4]));
-
-    return {
-        part1: part1(areas),
-        part2: part2(areas)
-    };
+    return { part1: collisions, part2: areaIds.values().next().value };
 }
