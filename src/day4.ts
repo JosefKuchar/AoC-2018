e70ab13bad54f4b2ba3b982b4734ecef7314051e
Josef Kuchař <josef.kuchar267@gmail.com>
2018-12-04 18:24:14 +0100

    Day 4

--- src/day4.ts
+++ src/day4.ts
@@ -1,10 +1,118 @@
-import * as moment from 'moment';
+import moment from "moment";
+
+enum Action {
+    Begin,
+    Fall,
+    Wake
+}
+
+class Record {
+    time: moment.Moment;
+    action: Action;
+    id: number;
+    constructor(time: moment.Moment, action: Action, id: number) {
+        this.time = time;
+        this.action = action;
+        this.id = id;
+    }
+}
+
+export function part1(guards: number[][]) {
+    const minutesAsleep = guards.map(a => a.reduce((x, y) => x + y));
+    let max = 0;
+    let maxId = 0;
+    minutesAsleep.forEach((minutes, index) => {
+        if (minutes > max) {
+            max = minutes;
+            maxId = index;
+        }
+    });
+
+    let maxMinute = 0;
+    max = 0;
+    guards[maxId].forEach((minute, index) => {
+        if (minute > max) {
+            max = minute;
+            maxMinute = index;
+        }
+    });
+
+    return maxMinute * maxId;
+}
+
+export function part2(guards: number[][]) {
+    let max = 0;
+    let maxMinute = 0;
+    let maxGuardId = 0;
+    guards.forEach((minutes, guardId) => {
+        minutes.forEach((frequency, minute) => {
+            if (frequency > max) {
+                max = frequency;
+                maxMinute = minute;
+                maxGuardId = guardId;
+            }
+        });
+    });
+
+    return maxMinute * maxGuardId;
+}
 
 export function solve(input: string) {
-    input.split('\n');
-    console.log(moment('1518-07-03 00:47', 'YYYY-MM-DD HH:mm').toDate());
+    let records = input
+        .split("\n")
+        .map(x => x.substring(1).split(/] | /))
+        .map(x => {
+            let action: Action = Action.Begin;
+            let id = 0;
+            switch (x[2]) {
+                case "Guard":
+                    action = Action.Begin;
+                    id = parseInt(x[3].substring(1));
+                    break;
+                case "falls":
+                    action = Action.Fall;
+                    break;
+                case "wakes":
+                    action = Action.Wake;
+                    break;
+            }
+            return new Record(
+                moment(x[0] + x[1], "YYYY-MM-DD HH:mm"),
+                action,
+                id
+            );
+        });
+
+    records.sort((x, y) => {
+        return x.time.diff(y.time) > 0 ? 1 : -1;
+    });
+
+    let guards: number[][] = new Array();
+    let id = 0;
+    records.forEach(record => {
+        if (record.action == Action.Begin) {
+            id = record.id;
+
+            // Fill guard hours if not yet
+            if (guards[id] == undefined) {
+                guards[id] = new Array();
+                for (let j = 0; j < 60; j++) {
+                    guards[id].push(0);
+                }
+            }
+        } else if (record.action == Action.Fall) {
+            for (let i = record.time.minute(); i < 60; i++) {
+                guards[id][i]++;
+            }
+        } else if (record.action == Action.Wake) {
+            for (let i = record.time.minute(); i < 60; i++) {
+                guards[id][i]--;
+            }
+        }
+    });
+
     return {
-        part1: 0,
-	part2: 0
+        part1: part1(guards),
+        part2: part2(guards)
     };
 }
--- test/day4.ts
+++ test/day4.ts
@@ -20,11 +20,15 @@ describe("Day 4", () => {
     '[1518-11-04 00:46] wakes up\n' +
     '[1518-11-05 00:03] Guard #99 begins shift\n' +
     '[1518-11-05 00:45] falls asleep\n' +
-    '[1518-11-05 00:55] wakes up\n';
+    '[1518-11-05 00:55] wakes up';
     
     const result = solve(input);
 
     it('Part 1', () => {
         expect(result.part1).to.equal(240);
+    });
+
+    it("Part 2", () => {
+        expect(result.part2).to.equal(4455);
     });
 });
