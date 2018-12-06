4dce9a61021e5c47c3b1df903a5277fa88f805dd
Josef Kuchař <josef.kuchar267@gmail.com>
2018-12-06 15:45:54 +0100

    Cleanup

--- src/day6.ts
+++ src/day6.ts
@@ -20,34 +20,11 @@ function closest(coordinates: number[][], x: number, y: number) {
     return multiple ? -1 : minIndex;
 }
 
-function part1() {
-
-}
-
-function part2(coordinates: number[][], lessThan: number) {
-    let total = 0;
-    for (let x = 0; x < 500; x++) {
-        for (let y = 0; y < 500; y++) {
-            let dst: number = 0;
-            coordinates.forEach(coordinate => {
-                dst += distance(x, y, coordinate[0], coordinate[1]);
-            });
-
-            if (dst < lessThan) {
-                total++;
-            }
-        }
-    }
-    return total;
-}
-
-export function solve(input: string, lessThan: number = 10000) {
-    let coordinates = input
-        .split('\n')
-        .map(x => x.split(', ').map(y => parseInt(y)));
-    let grid: number[][] = new Array(500)
+function part1(coordinates: number[][]) {
+    const size = 500;
+    let grid: number[][] = new Array(size)
         .fill(0)
-        .map(x => new Array(500).fill(-1));
+        .map(x => new Array(size).fill(-1));
     let finite = new Set();
 
     for (let i = 0; i < coordinates.length; i++) {
@@ -60,18 +37,18 @@ export function solve(input: string, lessThan: number = 10000) {
         }
     }
 
-    for (let i = 0; i < 500; i++) {
+    for (let i = 0; i < size; i++) {
         finite.delete(closest(coordinates, i, 0));
-        finite.delete(closest(coordinates, i, 500));
+        finite.delete(closest(coordinates, i, size));
         finite.delete(closest(coordinates, 0, i));
-        finite.delete(closest(coordinates, 500, i));
+        finite.delete(closest(coordinates, size, i));
     }
 
     let areas = new Array();
 
     finite.forEach(num => {
         areas[num] = 0;
-    });    
+    });
 
     for (let x = 0; x < grid.length; x++) {
         for (let y = 0; y < grid[0].length; y++) {
@@ -80,9 +57,29 @@ export function solve(input: string, lessThan: number = 10000) {
             });
         }
     }
+
+    return areas.reduce((acc, val) => (val > acc ? acc = val : acc));
+}
+
+function part2(coordinates: number[][], lessThan: number) {
+    let total = 0;
+    for (let x = 0; x < 500; x++) {
+        for (let y = 0; y < 500; y++) {
+            let dst = coordinates.reduce((acc, val) => distance(x, y, val[0], val[1]) + acc, 0);
+            if (dst < lessThan) total++;
+        }
+    }
+    return total;
+}
+
+export function solve(input: string, lessThan: number = 10000) {
+    let coordinates = input
+        .split('\n')
+        .map(x => x.split(', ').map(y => parseInt(y)));
+    
     
     return {
-        part1: areas.reduce((acc, val) => (val > acc ? acc = val : acc)),
+        part1: part1(coordinates),
         part2: part2(coordinates, lessThan)
     };
 }
