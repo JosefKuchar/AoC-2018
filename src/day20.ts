f07372a3f1c97741309d436b341387fc1f850a54
Josef Kuchař <josef.kuchar267@gmail.com>
2018-12-20 18:21:50 +0100

    Day 20 Part 1 stack size exceeded

--- src/day20.ts
+++ src/day20.ts
@@ -1,17 +1,17 @@
 enum Type {
     None,
-    NotSure,
     Door,
     Wall
 }
 
-function buildAround(grid: Type[][], x: number, y: number) {
-    const positions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-    positions.forEach(position => {
-        if (grid[x + position[0]][y + position[1]] == Type.None) {
-            grid[x + position[0]][y + position[1]] = Type.NotSure;
+function buildAround(grid: Type[][], posX: number, posY: number) {
+    for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+            if ((x != 0 || y != 0) && grid[x + posX][y + posY] == Type.None) {
+                grid[x + posX][y + posY] = Type.Wall;
+            }
         }
-    });
+    }
     return grid;
 }
 
@@ -46,15 +46,27 @@ function getOptions(input: string) {
     return options;
 }
 
-function buildRooms(input: string, grid: Type[][], posX: number, posY: number) {
+function generateNewPaths(paths: string[], add: string[]) {
+    let newPaths = new Array();
+    paths.forEach(path => {
+        add.forEach(addPath => {
+            newPaths.push(path + addPath);
+        });
+    });
+    return newPaths;
+}
+
+function buildPaths(input: string) {
     const options = getOptions(input);
+    let paths: string[] = new Array();
     if (options.length > 1) {
         options.forEach(option => {
-            grid = buildRooms(option, grid, posX, posY);
+            paths.push(...buildPaths(option));
         });
-        return grid;
+        return paths;
     }
 
+    paths.push('');
     let depth = 0;
     let buffer = '';
     for (let i = 0; i < input.length; i++) {
@@ -67,8 +79,7 @@ function buildRooms(input: string, grid: Type[][], posX: number, posY: number) {
             case ')':
                 depth--;
                 if (depth == 0) {
-                    //console.log(buffer);
-                    //TODO: Recursion
+                    paths = generateNewPaths(paths, buildPaths(buffer));
                     buffer = '';
                 } else {
                     buffer += char;
@@ -76,50 +87,101 @@ function buildRooms(input: string, grid: Type[][], posX: number, posY: number) {
                 break;
             default:
                 if (depth == 0) {
-                    let x = 0;
-                    let y = 0;
-                    switch (char) {
-                        case 'N':
-                            y--;
-                            break;
-                        case 'S':
-                            y++;
-                            break;
-                        case 'E':
-                            x++;
-                            break;
-                        case 'W':
-                            x--;
-                            break;
+                    for (let i = 0; i < paths.length; i++) {
+                        paths[i] += char;
                     }
-                    grid[posX + x][posY + y] = Type.Door;
-                    grid = buildAround(grid, posX, posY);
-                    posX += x * 2;
-                    posY += y * 2;
-                    grid = buildAround(grid, posX, posY);
                 } else {
                     buffer += char;
                 }
         }
     }
+
+    return paths;
+}
+
+function buildRooms(paths: string[]) {
+    let grid: Type[][] = new Array(200)
+        .fill(0)
+        .map(x => new Array(200).fill(Type.None));
+    paths.forEach(path => {
+        let posX = 100;
+        let posY = 100;
+
+        for (let i = 0; i < path.length; i++) {
+            let x = 0;
+            let y = 0;
+            switch (path.charAt(i)) {
+                case 'N':
+                    y--;
+                    break;
+                case 'S':
+                    y++;
+                    break;
+                case 'E':
+                    x++;
+                    break;
+                case 'W':
+                    x--;
+                    break;
+            }
+            grid[posX + x][posY + y] = Type.Door;
+            grid = buildAround(grid, posX, posY);
+            posX += x * 2;
+            posY += y * 2;
+            grid = buildAround(grid, posX, posY);
+        }
+    });
     return grid;
 }
 
 export function solve(input: string) {
-    let grid: Type[][] = new Array(20)
-        .fill(0)
-        .map(x => new Array(20).fill(Type.None));
-    buildRooms(input.substring(1, input.length - 1), grid, 10, 10);
+    const paths = buildPaths(input.substring(1, input.length - 1));
+    const grid = buildRooms(paths);
+    const searchGrid: number[][] = grid.map(x => x.map(y => y == Type.Wall ? -2 : -1));
+    
+    let queue = new Array();
+    const start = [100, 100];
+    queue.push(start);
+    searchGrid[start[0]][start[1]] = 0;
+    while (queue.length > 0) {
+        let current = queue.shift();
+        if (current == undefined) break;
+        let val = searchGrid[current[0]][current[1]];
+        if (searchGrid[current[0] - 1][current[1]] == -1) {
+            queue.push([current[0] - 1, current[1]]);
+            searchGrid[current[0] - 1][current[1]] = val + 1;
+        }
+        if (searchGrid[current[0] + 1][current[1]] == -1) {
+            queue.push([current[0] + 1, current[1]]);
+            searchGrid[current[0] + 1][current[1]] = val + 1;
+        }
+        if (searchGrid[current[0]][current[1] - 1] == -1) {
+            queue.push([current[0], current[1] - 1]);
+            searchGrid[current[0]][current[1] - 1] = val + 1;
+        }
+        if (searchGrid[current[0]][current[1] + 1] == -1) {
+            queue.push([current[0], current[1] + 1]);
+            searchGrid[current[0]][current[1] + 1] = val + 1;
+        }
+    }
 
+    let max = 0;
+    for (let x = 0; x < searchGrid.length; x++) {
+        for (let y = 0; y < searchGrid.length; y++) {
+            if (searchGrid[x][y] > max) {
+                max = searchGrid[x][y]
+            }
+        }
+    }
+
+    //console.log(searchGrid, max);
+    
     for (let y = 0; y < grid[0].length; y++) {
         let buffer = '';
         for (let x = 0; x < grid.length; x++) {
             switch(grid[x][y]) {
                 case Type.None:
                     buffer += '.'
-                    break;
-                case Type.NotSure:
-                    buffer += '?'
                     break;
                 case Type.Door:
                     buffer += '-'
@@ -131,10 +193,10 @@ export function solve(input: string) {
         console.log(buffer);
     }
 
-    console.log(getOptions('NEEE|'));
+    console.log('test');
 
     return {
-        part1: 0,
+        part1: max / 2,
         part2: 0
     };
 }
